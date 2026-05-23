6ac35881bdae793faeafe2916706c16599ddbe70
Samirborges <alessandro.samir@souunit.com.br>
2026-05-23 07:28:43 -0300

    style(login_page): Color and layout adjustment

--- localbiz/lib/login/login_page.dart
+++ localbiz/lib/login/login_page.dart
@@ -8,237 +8,264 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       
+      
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
         centerTitle: true,
         
         
-        title: const Text(
+        title: Padding(
+          padding: const EdgeInsets.only(top: 22),
+          child: Text(
           "LocalBiz",
           style: TextStyle(
             color: Color(0xFF64748B),
             fontWeight: FontWeight.bold,
-          ),
+          )
+        ),
         ),
 
         actions: [
-          IconButton(
+          Padding(
+            padding: const EdgeInsets.only(top: 22),
+            child: IconButton(
             icon: Icon(Icons.help_center_outlined),
             onPressed: () {
               ScaffoldMessenger.of(context).showSnackBar(
                 const SnackBar(content: Text("Ajuda ainda não implementada"))
               );
           }, 
+          ),
           )
         ],
       ),
 
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 24,
-              vertical: 16,
-            ),
-
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-
-              children: [
-                const SizedBox(height: 20,),
-
-                const Text(
-                  "Entrar",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-
-                const SizedBox(height: 8,),
-
-                const Text(
-                  "Bem-vindo de volta ao seu comércio local",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Color(0xFF64748B),
-                    fontSize: 14
-                  ),
-                ),
-
-                const SizedBox(height: 40),
-
-                const Text(
-                  "E-mail/Empresa",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-
-                const SizedBox(height: 8),
-
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: 12,
-                      vertical: 16,
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 1
+        ),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 16,
+              ),
+        
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+        
+                children: [
+                  const SizedBox(height: 14,),
+        
+                  const Text(
+                    "Entrar",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w900
                     ),
                   ),
-                ),
-                
-                const SizedBox(height: 24),
-
-                const Text(
-                  "Senha",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-
-                const SizedBox(height: 8),
-
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: 12,
-                      vertical: 16,
+        
+                  const SizedBox(height: 8,),
+        
+                  const Text(
+                    "Bem-vindo de volta ao seu comércio local",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color(0xFF64748B),
+                      fontSize: 14
                     ),
                   ),
-                ),
-                
-                const SizedBox(height: 12),
-
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "Esqueci minha senha",
-                      style: TextStyle(
-                        color: Color(0xFF64748B)
+        
+                  const SizedBox(height: 60),
+        
+                  const Text(
+                    "E-mail/Empresa",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+        
+                  const SizedBox(height: 8),
+        
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.zero
                       ),
-                    ),
-                  ),
-                ),
-
-                const SizedBox(height: 16),
-
-                SizedBox(
-                  height: 52,
-
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF64748B),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(0)
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 16,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.zero,
+                        borderSide: BorderSide(color: Color(0xFFE2E8F0))
                       )
                     ),
-
-                    onPressed: () {},
-
-                    child: const Text(
-                      "Entrar",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                      ),
+                  ),
+                  
+                  const SizedBox(height: 16),
+        
+                  const Text(
+                    "Senha",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
-                ),
-                
-                const SizedBox(height: 32),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "Não tenho conta, ",
+        
+                  const SizedBox(height: 8),
+        
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.zero
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 16,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.zero,
+                        borderSide: BorderSide(color: Color(0xFFE2E8F0))
+                      )
                     ),
-
-                    GestureDetector(
-                      onTap: () {},
-
+                  ),
+                  
+                  const SizedBox(height: 16),
+        
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {},
                       child: const Text(
-                        "cadastrar",
+                        "Esqueci minha senha",
                         style: TextStyle(
-                          fontWeight: FontWeight.bold,
                           color: Color(0xFF64748B)
                         ),
                       ),
                     ),
-                  ],
-                ),
-                
-                const SizedBox(height: 40),
-
-                Row(
-                  children: [
-                    Expanded(
-                      child: Divider(),
-                    ),
-
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 12,
+                  ),
+        
+                  const SizedBox(height: 40 ),
+        
+                  SizedBox(
+                    height: 60,
+        
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF043DAE),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0)
+                        )
                       ),
-
-                      child: Text(
-                        "OU ENTRE COM",
+        
+                      onPressed: () {},
+        
+                      child: const Text(
+                        "Entrar",
                         style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 12,
+                          color: Colors.white,
+                          fontSize: 16,
                         ),
                       ),
                     ),
-
-                    Expanded(
-                      child: Divider(),
-                    ),
-                  ],
-                ),
-
-                const SizedBox(height: 24),
-
-                SizedBox(
-                  height: 52,
-
-                  child: OutlinedButton(
-
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(0),
+                  ),
+                  
+                  const SizedBox(height: 56),
+        
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Não tenho conta, ",
                       ),
-                    ),
-                    
-                    onPressed: () {},
-
-                    child: const Text(
-                      "Google",
-                      style: TextStyle(
-                        color: Colors.black,
+        
+                      GestureDetector(
+                        onTap: () {},
+        
+                        child: const Text(
+                          "cadastrar",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF043DAE)
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  
+                  const SizedBox(height: 60),
+        
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Divider(),
+                      ),
+        
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 12,
+                        ),
+        
+                        child: Text(
+                          "OU ENTRE COM",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+        
+                      Expanded(
+                        child: Divider(),
+                      ),
+                    ],
+                  ),
+        
+                  const SizedBox(height: 36),
+        
+                  SizedBox(
+                    height: 52,
+        
+                    child: OutlinedButton(
+        
+                      style: OutlinedButton.styleFrom( // Ajustado para OutlinedButton
+                        side: const BorderSide(color: Color(0xFFE2E8F0)), // Define a cor da borda
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero, // Mantém quadrado
+                        ),
+                      ),
+                      
+                      onPressed: () {},
+        
+                      child: const Text(
+                        "Google",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold
+                        ),
                       ),
                     ),
                   ),
-                ),
-
-                const SizedBox(height: 50,),
-
-                const Text(
-                  "© 2024 LocalBiz. Todos os direitos reservados.",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 11
-                  ),
-                )
-                
-              ],
-            )
+        
+                  const SizedBox(height: 60,),
+        
+                  const Text(
+                    "© 2024 LocalBiz. Todos os direitos reservados.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 11
+                    ),
+                  )
+                  
+                ],
+              )
+            ),
           ),
         ),
       )
