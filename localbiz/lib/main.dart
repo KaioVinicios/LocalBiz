68a92e409c6ebef14c7155f00c66760af9fcf934
stheffanysantos <stheffany.sts@gmail.com>
2026-05-24 22:20:48 -0300

    feat: add navbar menu teste para navegação de paginas

--- localbiz/lib/main.dart
+++ localbiz/lib/main.dart
@@ -3,6 +3,10 @@ import 'package:localbiz/clientes/clientes_page.dart';
 import 'package:localbiz/forgot_password/forgot_password_page.dart';
 import 'package:localbiz/login/login_page.dart';
 import 'package:localbiz/register/register_page.dart';
+import 'package:localbiz/services/service_listing.dart';
+import 'package:localbiz/services/services_details.dart';
+import 'package:localbiz/pages/configuration/configuration_page.dart';
+import 'package:localbiz/widgets/nav_bar.dart';
 import 'package:localbiz/theme/app_colors.dart';
 
 void main() {
@@ -18,17 +22,41 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'LocalBiz',
       routes: {
+        '/home': (context) => const _HomeMenuTestePage(),
         '/login': (context) => const LoginPage(),
         '/recuperar-senha': (context) => const ForgotPasswordPage(),
         '/signin': (context) => const RegisterPage(),
         '/clientes': (context) => const ClientesPage(),
+        '/services': (context) => const ServicosScreen(),
+        '/service-details': (context) => const DetalheServicoScreen(),
+        '/configuration': (context) => const ConfigurationPage(),
       },
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.blue),
         scaffoldBackgroundColor: AppColors.surface,
       ),
-      home: const ClientesPage(),
+      home: const _HomeMenuTestePage(),
+    );
+  }
+}
+
+class _HomeMenuTestePage extends StatelessWidget {
+  const _HomeMenuTestePage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text(
+          'menu teste',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      bottomNavigationBar: const NavBar(initialIndex: 0),
     );
   }
 }
